00e43a2c16fa84f7c9466a22a25ed598ca0913ac
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2023-02-12 11:17:37 +0000

    Tuples, Points, and Vectors - Tuples

---
+++ test/RayTracerChallenge.Test/TuplesUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+
+namespace RayTracerChallenge.Test;
+
+public class TuplesUnitTest
+{
+    /// <summary>
+    /// A tuple with w=1.0 is a point
+    /// </summary>
+    [Theory]
+    [InlineAutoData(1.0)]
+    public void TupleIsAPoint(float w, float x, float y, float z)
+    {
+        var a = new Vector4(x, y, z, w);
+
+        a.X.Should().Be(x);
+        a.Y.Should().Be(y);
+        a.Z.Should().Be(z);
+        a.W.Should().Be(w);
+        a.IsPoint().Should().BeTrue();
+        a.IsVector().Should().BeFalse();
+    }
+
+    /// <summary>
+    /// A tuple with w=0 is a vector
+    /// </summary>
+    [Theory]
+    [InlineAutoData(0.0)]
+    public void TupleIsAVector(float w, float x, float y, float z)
+    {
+        var a = new Vector4(x, y, z, w);
+
+        a.X.Should().Be(x);
+        a.Y.Should().Be(y);
+        a.Z.Should().Be(z);
+        a.W.Should().Be(w);
+        a.IsPoint().Should().BeFalse();
+        a.IsVector().Should().BeTrue();
+    }
+}
---
+++ test/RayTracerChallenge.Test/Vector4Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace RayTracerChallenge.Test;
+
+public static class Vector4Extensions
+{
+    public static bool IsPoint(this Vector4 vector)
+        => Math.Abs(vector.W - 1.0f) < float.Epsilon;
+
+    public static bool IsVector(this Vector4 vector)
+        => Math.Abs(vector.W) < float.Epsilon;
+}
