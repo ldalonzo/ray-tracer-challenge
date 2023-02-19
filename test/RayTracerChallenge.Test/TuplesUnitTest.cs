a872b0ea51b249f386a7e94e7d930ee833ba574b
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2023-02-19 11:01:07 +0000

    Tuples, Points, and Vectors - Operations

--- test/RayTracerChallenge.Test/TuplesUnitTest.cs
+++ test/RayTracerChallenge.Test/TuplesUnitTest.cs
@@ -1,6 +1,4 @@
 ﻿using System.Numerics;
-using AutoFixture.Xunit2;
-using FluentAssertions;
 
 namespace RayTracerChallenge.Test;
 
@@ -38,5 +36,99 @@ public class TuplesUnitTest
         a.W.Should().Be(w);
         a.IsPoint().Should().BeFalse();
         a.IsVector().Should().BeTrue();
+    }
+
+    /// <summary>
+    /// point() creates tuples with w=1
+    /// </summary>
+    [Theory]
+    [AutoData]
+    public void PointFactory(float x, float y, float z)
+    {
+        var a = Point.Create(x, y, z);
+
+        a.X.Should().Be(x);
+        a.Y.Should().Be(y);
+        a.Z.Should().Be(z);
+        a.IsPoint().Should().BeTrue();
+        a.IsVector().Should().BeFalse();
+    }
+
+    /// <summary>
+    /// vector() creates tuples with w=0
+    /// </summary>
+    [Theory]
+    [AutoData]
+    public void VectorFactory(float x, float y, float z)
+    {
+        var a = Vector.Create(x, y, z);
+
+        a.X.Should().Be(x);
+        a.Y.Should().Be(y);
+        a.Z.Should().Be(z);
+        a.IsVector().Should().BeTrue();
+        a.IsPoint().Should().BeFalse();
+    }
+
+    [Fact]
+    public void AddingTwoTuples()
+    {
+        var a1 = new Vector4(3, -2, 5, 1);
+        var a2 = new Vector4(-2, 3, 1, 0);
+
+        var s = a1 + a2;
+
+        s.X.Should().Be(1);
+        s.Y.Should().Be(1);
+        s.Z.Should().Be(6);
+        s.W.Should().Be(1);
+    }
+
+    [Fact]
+    public void SubtractingTwoPoints()
+    {
+        var p1 = Point.Create(3, 2, 1);
+        var p2 = Point.Create(5, 6, 7);
+
+        var v = p1 - p2;
+
+        v.X.Should().Be(-2);
+        v.Y.Should().Be(-4);
+        v.Z.Should().Be(-6);
+        v.W.Should().Be(0);
+        v.IsPoint().Should().BeFalse();
+        v.IsVector().Should().BeTrue();
+    }
+
+    [Fact]
+    public void SubtractingVectorFromPoint()
+    {
+        var p = Point.Create(3, 2, 1);
+        var v = Vector.Create(5, 6, 7);
+
+        var q = p - v;
+
+        q.X.Should().Be(-2);
+        q.Y.Should().Be(-4);
+        q.Z.Should().Be(-6);
+        q.W.Should().Be(1);
+        q.IsPoint().Should().BeTrue();
+        q.IsVector().Should().BeFalse();
+    }
+
+    [Fact]
+    public void SubtractingTwoVectors()
+    {
+        var v1 = Vector.Create(3, 2, 1);
+        var v2 = Vector.Create(5, 6, 7);
+
+        var v3 = v1 - v2;
+
+        v3.X.Should().Be(-2);
+        v3.Y.Should().Be(-4);
+        v3.Z.Should().Be(-6);
+        v3.W.Should().Be(0);
+        v3.IsVector().Should().BeTrue();
+        v3.IsPoint().Should().BeFalse();
     }
 }
