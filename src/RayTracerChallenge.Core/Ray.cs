c2d9e485db9b01df4bf9a03766cbed9d2595c5a1
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2023-03-12 11:00:27 +0000

    Ray-Sphere Intersections - Creating Rays

---
+++ src/RayTracerChallenge.Core/Ray.cs
@@ -0,0 +1,9 @@
+﻿namespace RayTracerChallenge.Core;
+
+public record struct Ray(Vector4 Origin, Vector4 Direction)
+{
+    public Vector4 Position(float t)
+    {
+        return Origin;
+    }
+}
---
+++ test/RayTracerChallenge.Test/Features/Rays.cs
@@ -0,0 +1,31 @@
+﻿namespace RayTracerChallenge.Test.Features;
+
+public class Rays
+{
+    [Fact]
+    public void Creating_and_querying_a_ray()
+    {
+        var origin = Primitives.Point(1, 2, 3);
+        var direction = Primitives.Vector(4, 5, 6);
+        var ray = new Ray(origin, direction);
+
+        ray.Origin.Should().Be(origin);
+        ray.Direction.Should().Be(direction);
+    }
+
+    [Theory]
+    [InlineData(  0,   2, 3, 4)]
+    [InlineData(  1,   3, 3, 4)]
+    [InlineData( -1,   1, 3, 4)]
+    [InlineData(2.5, 4.5, 3, 4)]
+    public void Computing_a_point_from_a_distance(float t, float expX, float expY, float expZ)
+    {
+        var ray = new Ray(Primitives.Point(2, 3, 4), Primitives.Vector(1, 0, 0));
+
+        var p = ray.Position(t);
+        p.X.Should().Be(expX);
+        p.Y.Should().Be(expY);
+        p.Z.Should().Be(expZ);
+        p.IsPoint().Should().BeTrue();
+    }
+}
