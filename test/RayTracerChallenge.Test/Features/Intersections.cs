723410c4da0a5b63242bd32c3448212925a4cc1e
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2023-03-18 09:49:47 +0000

    Ray-Sphere Intersections - Identifying Hits

--- test/RayTracerChallenge.Test/Features/Intersections.cs
+++ test/RayTracerChallenge.Test/Features/Intersections.cs
@@ -24,4 +24,54 @@ public class Intersections
         xs.Should().NotBeEmpty();
         xs.Should().AllSatisfy(i => i.Object.Should().Be(s));
     }
+
+    [Fact]
+    public void The_hit_when_all_intersections_have_positive_t()
+    {
+        var s = new Sphere();
+        var i1 = new Intersection(s, 1);
+        var i2 = new Intersection(s, 2);
+
+        var i = new[] { i1, i2 }.Hit();
+
+        i.Should().Be(i1);
+    }
+
+    [Fact]
+    public void The_hit_when_some_intersections_have_negative_t()
+    {
+        var s = new Sphere();
+        var i1 = new Intersection(s, -1);
+        var i2 = new Intersection(s, 1);
+
+        var i = new[] { i1, i2 }.Hit();
+
+        i.Should().Be(i2);
+    }
+
+    [Fact]
+    public void The_hit_when_all_intersections_have_negative_t()
+    {
+        var s = new Sphere();
+        var i1 = new Intersection(s, -2);
+        var i2 = new Intersection(s, -1);
+
+        var i = new[] { i1, i2 }.Hit();
+
+        i.Should().BeNull();
+    }
+
+    [Fact]
+    public void The_hit_is_always_the_lowest_nonnegative_intersection()
+    {
+        var s = new Sphere();
+        var i1 = new Intersection(s, 5);
+        var i2 = new Intersection(s, 7);
+        var i3 = new Intersection(s, -3);
+        var i4 = new Intersection(s, 2);
+
+        var i = new[] { i1, i2, i3, i4 }.Hit();
+
+        i.Should().Be(i4);
+    }
 }
